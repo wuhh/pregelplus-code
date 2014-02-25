0ffa8c882bd84a29a7d5b9fd4746ec47ebd04f2a
Yi Lu <luyi0619@outlook.com>
2014-02-25 16:04:18 +0800

    type.h

--- utils/type.h
+++ utils/type.h
@@ -258,31 +258,4 @@ namespace __gnu_cxx
     };
 }
 
-//=============================================================
-
-struct triplet
-{
-	VertexID vid;
-	int bid;
-	int wid;
-
-	bool operator==(const triplet & o) const
-	{
-		return vid==o.vid;
-	}
-
-	friend ibinstream & operator<<(ibinstream & m, const triplet & idm){
-		m<<idm.vid;
-		m<<idm.bid;
-		m<<idm.wid;
-		return m;
-	}
-	friend obinstream & operator>>(obinstream &m, triplet & idm){
-		m>>idm.vid;
-		m>>idm.bid;
-		m>>idm.wid;
-		return m;
-	}
-};
-
 #endif /* TYPE_H_ */
