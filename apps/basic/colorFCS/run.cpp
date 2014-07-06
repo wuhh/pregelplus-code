1594a0caef354e394001528d80f79f4d158ce042
Yi Lu <luyi0619@outlook.com>
2014-07-06 13:46:17 +0800

    colorFCS

---
+++ apps/basic/colorFCS/run.cpp
@@ -0,0 +1,9 @@
+#include "pregel_app_colorFCS.h"
+
+int main(int argc, char* argv[])
+{
+    init_workers();
+    pregel_colorFCS("/pullgel/livej", "/exp/color");
+    worker_finalize();
+    return 0;
+}
