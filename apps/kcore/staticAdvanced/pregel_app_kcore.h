2ca77c60ee08af529d19f0d878ffe8114e37a079
Yi Lu <luyi0619@outlook.com>
2014-09-23 14:13:15 +0800

    hashmap @ kcore

--- apps/kcore/staticAdvanced/pregel_app_kcore.h
+++ apps/kcore/staticAdvanced/pregel_app_kcore.h
@@ -6,48 +6,37 @@
 using namespace std;
 const int inf = 1000000000;
 struct kcoreValue {
-    int K;
     vector<VertexID> edges;
-    vector<int> p;
 };
 
 ibinstream& operator<<(ibinstream& m, const kcoreValue& v)
 {
-    m << v.K;
     m << v.edges;
-    m << v.p;
     return m;
 }
 
 obinstream& operator>>(obinstream& m, kcoreValue& v)
 {
-    m >> v.K;
     m >> v.edges;
-    m >> v.p;
     return m;
 }
 
-//====================================
-bool intpaircmp(const intpair& p1, const intpair& p2)
-{
-    return p1.v1 < p2.v1; //id
-}
 class kcoreVertex : public Vertex<VertexID, kcoreValue, intpair> {
 public:
+    int phi;
+    hash_map<int,int> P;
     int subfunc(kcoreVertex* v)
     {
         vector<VertexID>& edges = v->value().edges;
-        vector<int>& p = v->value().p;
-        int& K = v->value().K;
 
-        vector<int> cd(K + 2, 0);
+        vector<int> cd(phi + 2, 0);
         for (int i = 0; i < edges.size(); i++) {
 
-            if (p[i] > K)
-                p[i] = K;
-            cd[p[i]]++;
+            if (P[i] > phi)
+                P[i] = phi;
+            cd[P[i]]++;
         }
-        for (int i = K; i >= 1; i--) {
+        for (int i = phi; i >= 1; i--) {
             cd[i] += cd[i + 1];
             if (cd[i] >= i)
                 return i;
@@ -57,31 +46,29 @@ public:
     virtual void compute(MessageContainer& messages)
     {
         vector<VertexID>& edges = value().edges;
-        vector<int>& p = value().p;
-        int& K = value().K;
         if (step_num() == 1) {
-            sort(edges.begin(), edges.end());
-            p = vector<int>(edges.size(), inf);
+            phi = edges.size();
             for (int i = 0; i < edges.size(); i++) {
-                send_message(edges[i], intpair(id, K));
+                send_message(edges[i], intpair(id, phi));
+                P[ edges[i] ] = inf;
             }
         } else {
-            sort(messages.begin(), messages.end(), intpaircmp);
-            // To be consistent with edges list;
-            for (int i = 0, j = 0; i < messages.size(); i++) {
-                while (edges[j] < messages[i].v1)
-                    j++;
-                if (messages[i].v2 < p[j])
-                    p[j] = messages[i].v2;
+            for (int i = 0; i < messages.size(); i++) {
+                int u = messages[i].v1;
+                int k = messages[i].v2;
+                if(P[u] > k)
+                {
+                    P[u] = k;
+                }
             }
 
             int x = subfunc(this); //
 
-            if (x < K) {
-                K = x;
+            if (x < phi) {
+                phi = x;
                 for (int i = 0; i < edges.size(); i++) {
-                    if (K < p[i]) {
-                        send_message(edges[i], intpair(id, K));
+                    if (phi < P[i]) {
+                        send_message(edges[i], intpair(id, phi));
                     }
                 }
             }
@@ -108,13 +95,12 @@ public:
             ssin >> nb;
             v->value().edges.push_back(nb);
         }
-        v->value().K = v->value().edges.size();
         return v;
     }
 
     virtual void toline(kcoreVertex* v, BufferedWriter& writer)
     {
-        sprintf(buf, "%d\t%d\n", v->id, v->value().K);
+        sprintf(buf, "%d\t%d\n", v->id, v->phi);
         writer.write(buf);
     }
 };
